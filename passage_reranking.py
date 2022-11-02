78fce9f23450be8b0a948e53221c710fd90ed0a0
DylanJoo <dylanjootw@gmail.com>
2022-11-02 10:46:36 +0800

    [fix] add cuda checking

--- passage_reranking.py
+++ passage_reranking.py
@@ -37,7 +37,8 @@ if __name__ == '__main__':
     model = monoT5.from_pretrained(args.model_name_or_path)
     model.set_tokenizer()
     model.set_targets()
-    model.to(f'cuda:{args.gpu}')
+    if torch.cuda.is_available():
+        model.to(f'cuda:{args.gpu}')
     model.eval()
 
     # load triplet
