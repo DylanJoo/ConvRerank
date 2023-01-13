c12209d21be8e380283f0205e6b282d38715f306
DylanJoo <dylanjootw@gmail.com>
2023-01-13 18:28:23 +0800

    [stats] add the stats of canard dist.

--- run_create_convir_dataset.sh
+++ run_create_convir_dataset.sh
@@ -1,9 +1,25 @@
+mkdir -p data/canard4ir/
+
+# Setting 1: view1 - view0
 python3 tools/construct_convir_dataset.py \
-  --topic data/canard.train.jsonl \
-  --run_teacher runs/cast20.canard.train.answer+rewrite.spr.top1000.trec \
-  --run_student runs/cast20.canard.train.rewrite.spr.top1000.trec  \
-  --output data/convir/convir.train.convrerank.jsonl \
+  --topic data/canard/train.jsonl \
+  --collections /tmp2/jhju/datasets/cast2020/ \
+  --run0 runs/cast20.canard.train.view0.monot5.top1000.trec \
+  --run1 runs/cast20.canard.train.view1.monot5.top1000.trec \
+  --output data/canard4ir/canard4ir.train.convrerank.txt \
+  --window_size 3 \
   --topk_pool 200 \
   --topk_positive 3 \
   --n 30 \
-  --collections /tmp2/trec/cast/2020/cast2020_psg/
+
+# Setting 2: view0 - viewx
+# python3 tools/construct_convir_dataset.py \
+#   --topic data/canard/train.jsonl \
+#   --collections /tmp2/jhju/datasets/cast2020/ \
+#   --run0 runs/cast20.canard.train.viewx.monot5.top1000.trec \
+#   --run1 runs/cast20.canard.train.view0.monot5.top1000.trec \
+#   --output data/canard4ir/canard4ir.train.convrerank.setting2.txt \
+#   --window_size 3 \
+#   --topk_pool 200 \
+#   --topk_positive 3 \
+#   --n 30 \
--- tools/construct_convir_dataset.py
+++ tools/construct_convir_dataset.py
@@ -4,7 +4,7 @@ import random
 import collections
 import json
 from tqdm import tqdm
-from utils import load_runs, load_collections, load_topics, normalized
+from utils import load_runs, load_collections, load_topics, normalized, doc_pool_random_sampling
 
 
 def conversational_pseudo_labeling(args):
@@ -14,25 +14,26 @@ def conversational_pseudo_labeling(args):
     run1 = load_runs(args.run1)
     passages = load_collections(dir=args.collections)
     assert len(run0) == len(run1), f"Inconsistent number of queries: len(run0) and len(run1)"
+    print('resource loaded')
 
     random.seed(123)
-    fout = open(args.output, 'w')
-    with open(args.topic) as topic:
+    with open(args.output, 'w') as fout, \
+         open(args.output.replace('txt', 'stats'), 'w') as fstat:
+
         for qid in tqdm(queries):
-
-            qtext = queries[qid]
-
+            turn_i = qid.split('#')[-1]
+            query = queries[qid]
             # view0 and view1 ranked list
-            ranklist_view0 = [docid for docid in run_view0[qid][:args.topk_pool]]
-            ranklist_view1 = [docid for docid in run_view1[qid][:args.topk_pool]]
+            ranklist_view0 = [docid for docid in run0[qid][:args.topk_pool]]
+            ranklist_view1 = [docid for docid in run1[qid][:args.topk_pool]]
 
             # Pseudo positive/negative based on view0
             ## in_pool: collect the easy positive candidates
             ## out_pool: collect the harder negative candidates
+            # [CONCERN] out_pool and in_pool order
             in_pool = [docid for docid in ranklist_view0 if docid in ranklist_view1]
             out_pool = [docid for docid in ranklist_view0 if docid not in ranklist_view1]
             positive_pool = (in_pool+out_pool)[:args.topk_positive]
-            # [CONCERN] out_pool and in_pool order
             negative_pool = (in_pool+out_pool)[args.topk_positive:]
 
             # sampling positives and negatives, n is the number of triplets for each topic
@@ -40,24 +41,24 @@ def conversational_pseudo_labeling(args):
             psg_ids_neg = doc_pool_random_sampling(negative_pool, args.n)
             
             # the conversational query
-            ## q: current turn of information need
+            ## 0: current turn of information need
             ## c_t: detail topic of historical information need
             ## c_u: historical information need
-            q = normalized(query_dict['utterance'])
-            c_t = "|".join(query_dict['history_topic'])
-            c_u = query_dict['history_utterances'][-args.window_size:]
+            ## w: the window size of historical utterance
+            a = normalized(query['answer'])
+            q = normalized(query['utterance'])
+            c_t = "|".join(query['history_topic'])
+            c_u = query['history_utterances'][-args.window_size:]
             c = normalized("|".join([c_t] + c_u))
 
-            for j, (psg_id_pos, psg_id_neg) in enumerate(zip(psg_ids_pos, psg_ids_neg)):
+            for psg_id_pos, psg_id_neg in zip(psg_ids_pos, psg_ids_neg):
                 d_pos = normalized(passages[psg_id_pos])
                 d_neg = normalized(passages[psg_id_neg])
                 fout.write(f"Query: {q} Context: {c} Document: {d_pos} Relevant:\ttrue\n")
                 fout.write(f"Query: {q} Context: {c} Document: {d_neg} Relevant:\tfalse\n")
 
-        if i % 10000 == 0:
-            print(f"{i} convir queries finished...")
-
-    fout.close()
+            # append stat
+            fstat.write(f"{turn_i}, {len(in_pool)}, {len(out_pool)}, {q}, {a}\n")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -71,4 +72,6 @@ if __name__ == '__main__':
     parser.add_argument("--window_size", type=int, default=0)
     parser.add_argument("--collections", type=str, default="/tmp2/jhju/datasets/cast2020")
     args = parser.parse_args()
+
+    conversational_pseudo_labeling(args)
 
