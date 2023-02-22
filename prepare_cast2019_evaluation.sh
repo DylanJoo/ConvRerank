54980ccabfdc0d2e4edfab833fe38ac79d58cf33
DylanJoo <dylanjootw@gmail.com>
2023-02-22 10:26:26 +0800

    [results] update results

--- prepare_cast2019_evaluation.sh
+++ prepare_cast2019_evaluation.sh
@@ -1,27 +1,38 @@
 mkdir -p monot5/cast2019
 
 # convert to monot5 
-for first_stage in hqe;do
-    run=runs/cast2019/cast2019.eval.${first_stage}.trec
-    file=${run##*/}
-    python3 tools/convert_runs_to_monot5.py \
-      --run $run \
-      --topics data/cast2019/cast2019.eval.jsonl \
-      --collection /tmp2/jhju/datasets/cast2020 \
-      --output monot5/cast2019/${file/trec/rerank.txt}
-done &
+# for first_stage in cqe;do
+#     run=runs/cast2019/cast2019.eval.${first_stage}.trec
+#     file=${run##*/}
+#     python3 tools/convert_runs_to_monot5.py \
+#       --run $run \
+#       --topics data/cast2019/cast2019.eval.jsonl \
+#       --collection /tmp2/jhju/datasets/cast2020 \
+#       --output monot5/cast2019/${file/trec/rerank.txt}
+# done &
 
 # convert to conversational monot5
-for first_stage in hqe;do
-    run=runs/cast2019/cast2019.eval.${first_stage}.trec
-    file=${run##*/}
-    python3 tools/convert_runs_to_monot5.py \
-      --run $run \
-      --topics data/cast2019/cast2019.eval.jsonl \
-      --collection /tmp2/jhju/datasets/cast2020 \
-      --output monot5/cast2019/${file/trec/conv.rerank.txt} \
-      --conversational
-done
+# for first_stage in cqe;do
+#     run=runs/cast2019/cast2019.eval.${first_stage}.trec
+#     file=${run##*/}
+#     python3 tools/convert_runs_to_monot5.py \
+#       --run $run \
+#       --topics data/cast2019/cast2019.eval.jsonl \
+#       --collection /tmp2/jhju/datasets/cast2020 \
+#       --output monot5/cast2019/${file/trec/conv.rerank.txt} \
+#       --conversational
+# done
+
+# upper bound
+# for first_stage in manual.dpr;do
+#     run=runs/cast2019/cast2019.eval.${first_stage}.trec
+#     file=${run##*/}
+#     python3 tools/convert_runs_to_monot5.py \
+#       --run $run \
+#       --topics data/cast2019/cast2019.eval.jsonl \
+#       --collection /tmp2/jhju/datasets/cast2020 \
+#       --output monot5/cast2019/${file/trec/rerank.txt}
+# done &
 
 # using train set
 # run=runs/cast2019/cast2019.train.cqe.trec
--- test.sh
+++ test.sh
@@ -3,15 +3,15 @@ mkdir -p runs/test
 # Main-A: monot5 rerank & Convrerank
 for k in 10 20 40 50 100 300 500 1000;do
     python3 tools/rerank_runs.py \
-        --baseline runs/cast2019/cast2019.eval.cqe.trec \
-        --scores monot5-probs/rerank_cast2019/cast2019.eval.cqe.rerank.txt.probs \
+        --baseline runs/cast2019/cast2019.eval.hqe.trec \
+        --scores monot5-probs/rerank_cast2019/ablation_firststage/cast2019.eval.hqe.rerank.txt.probs \
         --reranked runs/test/monot5-$k.trec \
         --topk $k \
         --prefix monot5 
 
     python3 tools/rerank_runs.py \
-        --baseline runs/cast2019/cast2019.eval.cqe.trec \
-        --scores monot5-probs/rerank_cast2019/cast2019.eval.cqe.conv.rerank.t40.txt.probs \
+        --baseline runs/cast2019/cast2019.eval.hqe.trec \
+        --scores monot5-probs/rerank_cast2019/ablation_firststage/cast2019.eval.hqe.conv.rerank.txt.probs \
         --reranked runs/test/convrerank-$k.trec \
         --topk $k \
         --prefix conv-monot5 
